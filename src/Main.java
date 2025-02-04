d7032f8a47e2ff6fbf116a628bdb6c1f9a43f844
rayyanr08 <rayyanrahman26@gmail.com>
2025-02-04 09:53:21 -0500

    Update Main.java
    Took out methods that weren't supposed to be in main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
@@ -46,29 +45,10 @@ public class Main {
                 hands.add(hand);
             }
 
-            // Sort the hands based on strength
-            hands.sort((h1, h2) -> {
-                int typeComparison = Integer.compare(getHandTypeStrength(h2.getType()), getHandTypeStrength(h1.getType()));
-                if (typeComparison != 0) {
-                    return typeComparison;
-                }
-                // If types are equal, compare card by card
-                for (int i = 0; i < 5; i++) {
-                    int cardComparison = Integer.compare(getCardStrength(h2.getCards()[i]), getCardStrength(h1.getCards()[i]));
-                    if (cardComparison != 0) {
-                        return cardComparison;
-                    }
-                }
-                return 0;
-            });
+            Hand.sortArray(hands);
 
-            // Calculate total bid value
-            int totalValue = 0;
-            int rank = hands.size();
-            for (Hand line : hands){
-                totalValue += line.getBid() * rank;
-                rank --;
-            }
+            // Calculate total bid value\
+            Hand.calculateBid(hands);
 
             // Print Part 1 results
 
@@ -81,7 +61,7 @@ public class Main {
             System.out.println("Number of high card hands: " + highCard);
 
             // Print Part 2 result
-            System.out.println("Total Bid Value: " + totalValue);
+            System.out.println("Total Bid Value: " + Hand.getTotalBid());
 
         } catch (FileNotFoundException fe) {
             System.out.println("File was not found");
@@ -90,40 +70,5 @@ public class Main {
 
 
     }
-
-    // Helper method to get the strength of a hand type
-    private static int getHandTypeStrength(String type) {
-        return switch (type) {
-            case "Five of a kind" -> 7;
-            case "Four of a kind" -> 6;
-            case "Full house" -> 5;
-            case "Three of a kind" -> 4;
-            case "Two pair" -> 3;
-            case "One pair" -> 2;
-            case "High card" -> 1;
-            default -> 0;
-        };
-    }
-
-    // Helper method to get the strength of a card
-    private static int getCardStrength(String card) {
-        return switch (card) {
-            case "Ace" -> 14;
-            case "King" -> 13;
-            case "Queen" -> 12;
-            case "Jack" -> 1;
-            case "10" -> 10;
-            case "9" -> 9;
-            case "8" -> 8;
-            case "7" -> 7;
-            case "6" -> 6;
-            case "5" -> 5;
-            case "4" -> 4;
-            case "3" -> 3;
-            case "2" -> 2;
-            default -> 0;
-        };
-    }
-
 
 }
